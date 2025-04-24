e6eacf878463dffe9aaf456de9fd19ccfea10360
Ganbayr123 <62046861+Ganbayr123@users.noreply.github.com>
2025-04-24 12:53:08 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-export default function Home() {
+function Home() {
   return (
     <div>
       <h1>Welcome to Angu Website</h1>
@@ -6,3 +6,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
