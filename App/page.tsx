0b3a674cdff1e1cacd6ee3cf40783fac68478839
Ganbayr123 <62046861+Ganbayr123@users.noreply.github.com>
2025-04-24 11:49:39 +0800

    Delete App directory

--- App/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center h-screen bg-black text-white">
-      <h1 className="text-4xl font-bold">Angu — Fashion That Moves</h1>
-    </main>
-  )
-}
