14677801c3d977de4bcb9faa9c0925129e8b2aee
Ganbayr123 <62046861+Ganbayr123@users.noreply.github.com>
2025-04-24 12:52:17 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ export const metadata = {
   description: 'Welcome to the Angu Website',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
